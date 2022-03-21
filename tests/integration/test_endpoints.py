2e9350639a1121e0658f7793bdb806f4bdad5e08
Kenny Lajara <kennylajara@gmail.com>
2022-03-21 03:45:46 -0400

    Update unit tests of endpoint GET /display/:id

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -15,23 +15,30 @@ def startup():
         os.remove(filename)
 
 
-def test_endpoint_display_with_invalid_id():
+#
+#  GET /display/{id}
+#
+
+
+def test_endpoint_display_with_non_existing_id():
     response = client.get("/display/invalid")
     assert response.status_code == 404
     assert response.headers["content-type"] == "application/json"
     assert response.json() == {"detail": "Image not found in database"}
 
 
-def test_endpoint_display_with_downloaded_image():
-    response = client.get("/display/f1650f2a99824f349643ad234abff6a2")
-    assert response.status_code == 200
-    assert response.headers["content-type"] == "image/jpg"
+def test_endpoint_display_with_existing_id():
+    # Two times: one with the image in the database that needs to
+    # be downloaded, and one after downloading the image.
+    for _ in range(2):
+        response = client.get("/display/f1650f2a99824f349643ad234abff6a2")
+        assert response.status_code == 200
+        assert response.headers["content-type"] == "image/jpg"
 
 
-def test_endpoint_display_with_not_downloaded_image():
-    response = client.get("/display/f853874999424ad2a5b6f37af6b56610")
-    assert response.status_code == 200
-    assert response.headers["content-type"] == "image/jpg"
+#
+#  POST /find
+#
 
 
 def test_endpoint_find_with_invalid_geojson():
